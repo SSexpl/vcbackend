397efc487e5ca97172828b2808929b28c0c836bc
SSexpl <87913198+SSexpl@users.noreply.github.com>
2023-07-24 07:07:55 +0530

    1.0

---
+++ db.js
@@ -0,0 +1,22 @@
+const { Client } = require('pg');
+
+const h=process.env.host;
+const p=process.env.port;
+const db=process.env.database;
+const us=process.env.user;
+const ps=process.env.password;
+// Create a new PostgreSQL client
+const client = new Client({
+  host: h,
+  port: p,
+  database:db,
+  user: us,
+  password: ps,
+});
+
+// Connect to the database
+client.connect()
+  .then(() => console.log('Connected to the PostgreSQL database'))
+  .catch((err) => console.error('Error connecting to the database', err));
+
+module.exports = client;
---
+++ models/create_db.js
@@ -0,0 +1,37 @@
+const client= require('../db');
+
+// const createTableQuery = `
+// CREATE TABLE IF NOT EXISTS students (
+//     ID SERIAL,
+//     name VARCHAR(255) NOT NULL,
+//     email VARCHAR(255) PRIMARY KEY,
+//     registration_number VARCHAR(20) NOT NULL,
+//     hostel_block VARCHAR(50),
+//     room_number VARCHAR(10)
+//   );
+  
+// `;
+
+
+
+// const createTableQuery=`CREATE TABLE IF NOT EXISTS otps (
+//   email VARCHAR(255) REFERENCES students(email),
+//   otp VARCHAR(6) NOT NULL,
+//   expiry_time TIMESTAMPTZ NOT NULL
+// );`
+
+const createTableQuery=`CREATE TABLE IF NOT EXISTS requests (
+  
+  req_id SERIAL PRIMARY KEY,
+  hostel_block VARCHAR(255) NOT NULL,
+  room_number VARCHAR(20) NOT NULL,
+  type VARCHAR(50) ,
+  date DATE NOT NULL,
+  status VARCHAR(20) NOT NULL,
+  verification_code CHAR(6) NOT NULL
+);`
+// Execute the SQL query to create the table
+client.query(createTableQuery)
+  .then(() => console.log('Created the "requests new " table'))
+  .catch((err) => console.error('Error creating the table', err))
+  .finally(() => client.end()); // Close the database connection after executing the query
